a0e3cad026c33343f9417b0b613009b668afa242
Shirley Brito <shirley.omb4@gmail.com>
2021-01-26 14:31:02 -0300

    second_upload

--- functionseoperadores.js
+++ functionseoperadores.js
@@ -1,3 +1,4 @@
+/*
 function fn() {
   return 'Code here';
 }
@@ -19,3 +20,42 @@ const logValue = value => console.log(value);
 const logFnResult = fnParam => console.log(fnParam());
 
 logFnResult(fn);
+
+//Receber e retornar funções
+const controlFnExec=  fnParam => allowed => {
+  if (allowed){
+    fnParam();
+  }
+}
+
+const handleFnExecution = controlFnExec(fn);
+
+//ControlFnExec como função
+function controlFnExec(fnParam){
+  return function(allowed) {
+    if (allowed){
+      fnParam();
+    }
+  }
+}
+*/
+
+(() => {
+this.name = 'Arrow function';
+
+const getNameArrowFn = () => this.name;
+
+function getName(){
+  return this.name;
+}
+
+const user = {
+  name: 'Nome no objeto de execução',
+  getNameArrowFn,
+  getName
+}
+
+console.log(user.getNameArrowFn());
+console.log(user.getName());
+})();
+
